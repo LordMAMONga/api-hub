285daf412f0fc6971b0df700a7d994a263ec2e0e
emir-dev312 <emir.dev312@gmail.com>
2026-04-20 01:30:57 +0600

    feat: реализован бесконечный скролл и редизайн Мяу Базы

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,14 +1,21 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { Layout } from "../../core/components/Layout";
+
+// Импорты Pokedex
 import { PokemonListScreen } from "../../features/pokedex/ui/screens/PokemonListScreen";
 import { PokemonDetailScreen } from "../../features/pokedex/ui/screens/PokemonDetailScreen";
 
+// Импорты Anime
 import { AnimePage } from "../../features/jikan/ui/AnimePage";
 import { AnimeDetailScreen } from "../../features/jikan/ui/screens/AnimeDetailScreen/AnimeDetailScreen";
 
+// ИСПРАВЛЕННЫЙ ИМПОРТ: Путь к котам без пробелов
+// Важно: если папка называется cats (маленькими), то и здесь должно быть cats
+import { CatsPage } from "../../features/cats/ui/screens/CatsPage";
+
 const HomePage = () => (
   <div style={{ padding: "20px" }}>
-    <h1>API Hub </h1>
+    <h1>API Hub</h1>
     <p>Выберите проект:</p>
     <ul style={{ fontSize: "20px", lineHeight: "1.8" }}>
       <li>
@@ -23,6 +30,9 @@ const HomePage = () => (
       <li>
         <Link to="/crypto">4. Crypto Dashboard (Разработчик 3)</Link>
       </li>
+      <li>
+        <Link to="/cats">5. Cats Database (Эмир)</Link>
+      </li>
     </ul>
   </div>
 );
@@ -33,13 +43,18 @@ export const AppRouter = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
 
+        {/* Секция Покемонов */}
         <Route path="pokemon" element={<PokemonListScreen />} />
         <Route path="pokemon/:name" element={<PokemonDetailScreen />} />
 
+        {/* Секция Аниме */}
         <Route path="anime">
           <Route index element={<AnimePage />} />
           <Route path=":id" element={<AnimeDetailScreen />} />
         </Route>
+
+        {/* ТВОЯ СЕКЦИЯ: Коты */}
+        <Route path="cats" element={<CatsPage />} />
 
       </Route>
     </Routes>
